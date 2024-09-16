a235c93e065601b53f0c856cdb408a814beb947b
samarth patel <samarthpatel981@gmail.com>
2024-09-16 11:11:43 +0530

    new file

---
+++ DataStructure/stack/stackSpan.java
@@ -0,0 +1,31 @@
+import  java.util.*;
+
+public class stackSpan {
+    public static void stockspan(int stack[], int span[]){
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+        for(int i=1;i<stack.length;i++){
+            int currnt = stack[i];
+            while(!s.isEmpty() && currnt > stack[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                span[i] = i+1;
+            }else{
+                int prevHigh = s.peek();
+                span[i] = i-prevHigh;
+            }
+            s.push(i);
+        }
+    }
+    public static void main(String args[]){
+        int stack[] = {100,80,60,70,60,85,100};
+        int span[] = new int[stack.length];
+        stockspan(stack,span);
+
+        for(int i=0 ; i<span.length; i++){
+            System.out.print(span[i] + " ");
+        }
+    }
+}
